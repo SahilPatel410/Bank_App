a5cf61748dc6fa248529754f56fa07df4941c0d7
Mehul Trivedi <pci126@server7.com>
2019-02-07 09:15:51 +0530

    letest

--- src/accounts/Account.java
+++ src/accounts/Account.java
@@ -1,8 +1,12 @@
-package accounts;
-import dao.IEmployeeDao;
-import dao_imp.EmployeeDao;
+package account;
+import dao_imp.*;
+public class account {
+	
+	public static void main(String args[])
+	{
+		EmployeeDao obj =new EmployeeDao();
+		obj.add_Account();
+		obj.
+	}
 
-public class Accounts {
-	
-	
 }
